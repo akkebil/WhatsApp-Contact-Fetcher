d079a84bf79d0f2f1381cc0ac953495c6fe5bef2
akkebil <38065447+akkebil@users.noreply.github.com>
2023-05-20 02:23:51 +0200

    Delete popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("run-script").addEventListener("click", () => {
-  console.log("Button clicked");  // Ny rad
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    console.log("Running script");  // Ny rad
-    chrome.tabs.executeScript(tabs[0].id, { file: "contentScript.js" });
-  });
-});
